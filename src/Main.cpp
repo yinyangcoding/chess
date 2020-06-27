496246387d9a27c23d99e3129991ea531890e0b9
Matt London <mattlondon57@gmail.com>
2020-06-26 22:50:07 -0400

    Switch to CIN, solves issue

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,12 +18,17 @@ using namespace std;
 
 int main() {
 	Board b(true);
+
+	int fromI, toI;
+	char fromC, toC;
+
 	while(true) {
-		int fromI, toI;
-		char fromC, toC;
 		b.print_board();
-		// b.print_id();
-		scanf("%c%d %c%d", &fromC, &fromI, &toC, &toI);
+		cin >> fromC;
+		cin >> fromI;
+		cin >> toC;
+		cin >> toI;
+
 		Move::move(b, Coordinate(fromC, fromI), Coordinate(toC, toI));
 		printf("\n");
 	}

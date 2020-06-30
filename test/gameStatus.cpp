5c5d88afc6b7ee9165c66b8e0a470d3ad417e4e2
Matt London <mattlondon57@gmail.com>
2020-06-29 22:16:30 -0400

    Save gameStatus test

---
+++ test/gameStatus.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "../include/resources/Constants.h"
+#include "../include/objects/Coordinate.hpp"
+#include "../include/objects/Piece.hpp"
+#include "../include/resources/Tools.hpp"
+#include "../include/objects/Board.hpp"
+#include "../include/objects/Player.hpp"
+#include "../include/resources/Move.hpp"
+
+using namespace std;
+
+int main() {
+	Board b(true);
+
+	int fromI, toI;
+	char fromC, toC;
+
+	while(true) {
+		b.print_board();
+		cin >> fromC;
+		cin >> fromI;
+		cin >> toC;
+		cin >> toI;
+
+		Move::move(b, Coordinate(fromC, fromI), Coordinate(toC, toI));
+		cout << "White: " << Move::game_status(b, 'w') << endl;
+		cout << "Black: " << Move::game_status(b, 'b') << endl;
+		printf("\n");
+	}
+
+
+	return 0;
+}

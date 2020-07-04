00fb2c62394773e5f8a3812e37aad78301a33fdf
Matt London <mattlondon57@gmail.com>
2020-07-04 17:17:27 -0400

    Change to namespace

--- include/resources/BlandTools.hpp
+++ include/resources/BlandTools.hpp
@@ -8,19 +8,18 @@
 
 
 // Contains tools that are needed universally throughout code
-class BTools {
-    public:
-        // Toupper and tolower for chars
-        static char to_upper(char c);
-        static char to_lower(char c);
+namespace BTools {
+    // Toupper and tolower for chars
+    char to_upper(char c);
+    char to_lower(char c);
 
-        // Trim functions for strings
-        static std::string trim(std::string str);
-        static std::string ltrim(std::string str);
-        static std::string rtrim(std::string str);
+    // Trim functions for strings
+    std::string trim(std::string str);
+    std::string ltrim(std::string str);
+    std::string rtrim(std::string str);
 
-        // Easy debug step
-        static void debug(std::string s);
+    // Easy debug step
+    void debug(std::string s);
 
 };
 

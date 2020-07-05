0a7a10b9c37f51cc0cde79930b1f9f951ffd0f8e
Matt London <mattlondon57@gmail.com>
2020-07-05 15:33:17 -0400

    Time test of setting a hasMoved variable on each turn

---
+++ test/setBool.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#define testNum 1000000000
+
+int main() {
+	bool test = false;
+	for (int i = 0; i < testNum; i++) {
+		//if (!test) {
+			test = true;
+		//}
+	}
+
+	return 0;
+}
